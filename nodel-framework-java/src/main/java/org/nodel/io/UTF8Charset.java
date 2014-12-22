2737f9eb5fd0179d52875fe4faea26fb22ea025a
justparking <justparking@gmail.com>
2014-12-23 10:14:23 +1100

    simplified UTF8 singleton.

--- nodel-framework-java/src/main/java/org/nodel/io/UTF8Charset.java
+++ nodel-framework-java/src/main/java/org/nodel/io/UTF8Charset.java
@@ -6,23 +6,23 @@ import java.nio.charset.Charset;
  * A fast lookup class for the UTF8 character set.
  */
 public class UTF8Charset {
-    
+
     private Charset _charset = Charset.forName("UTF8");
-    
+
     private UTF8Charset() {
         _charset = Charset.forName("UTF8");
     }
-    
+
     private static class LazyHolder {
         private static final UTF8Charset INSTANCE = new UTF8Charset();
     }
- 
-    public static UTF8Charset instance() {
-        return LazyHolder.INSTANCE;
+
+    public static Charset instance() {
+        return LazyHolder.INSTANCE.charset();
     }
-    
-    public Charset charset() {
-        return _charset;        
+
+    private Charset charset() {
+        return _charset;
     }
 
 } // (class)
