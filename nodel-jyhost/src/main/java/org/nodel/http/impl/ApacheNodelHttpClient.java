3d6f25a4592ad60131509ecb075b8897b3241375
justparking <justparking@gmail.com>
2018-02-16 16:04:58 +1100

    addresses issue 25 in https://github.com/museumsvictoria/nodel-recipes

--- nodel-jyhost/src/main/java/org/nodel/http/impl/ApacheNodelHttpClient.java
+++ nodel-jyhost/src/main/java/org/nodel/http/impl/ApacheNodelHttpClient.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -13,34 +15,22 @@ import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
 import org.apache.http.StatusLine;
-import org.apache.http.auth.AuthSchemeRegistry;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.params.CookiePolicy;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.protocol.HTTP;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.nodel.Strings;
-import org.nodel.core.Nodel;
 import org.nodel.io.Stream;
 import org.nodel.io.UnexpectedIOException;
 import org.nodel.json.JSONObject;
@@ -48,58 +38,80 @@ import org.nodel.net.NodelHTTPClient;
 
 public class ApacheNodelHttpClient extends NodelHTTPClient {
     
+    private Object _lock = new Object();
+    
     /**
-     * The Apache 'DefaultHttpClient'
+     * The Apache Http Client
+     * (created lazily)
      */
-    private DefaultHttpClient _httpClient;
+    private CloseableHttpClient _httpClient;
     
-    public ApacheNodelHttpClient() {
-        _httpClient = new DefaultHttpClient();
+    /**
+     * Required with 'applySecurity'
+     */
+    private CredentialsProvider _credentialsProvider;
+
+    /**
+     * Mainly used for adjusting timeouts
+     */
+    private RequestConfig _requestConfig;
+
+    /**
+     * This needs to be done lazily because proxy can only be set up once
+     * 
+     * (uses double-check singleton)
+     */
+    private void tryInitHttpClient(String proxyAddress, String proxyUsername, String proxyPassword) {
+        if (_httpClient == null) {
+            synchronized (_lock) {
+                if (_httpClient != null)
+                    return;
+                
+                HttpClientBuilder httpClientBuilder = HttpClients.custom()
+                        
+                        // need to reference this later
+                        .setDefaultCredentialsProvider(_credentialsProvider = new BasicCredentialsProvider())
+                        
+                        // default timeouts
+                        .setDefaultRequestConfig(_requestConfig = RequestConfig.custom()
+                                .setConnectTimeout(DEFAULT_CONNECTTIMEOUT)
+                                .setSocketTimeout(DEFAULT_READTIMEOUT)
+                                .build());
+                
+                // using a proxy?
+                if (proxyAddress != null) {
+                    String[] proxyAddressParts = proxyAddress.split(":");
+                    if (proxyAddressParts.length != 2)
+                        throw new IllegalArgumentException("Proxy address is not in form host:port");
+                    
+                    String proxyHost = proxyAddressParts[0];
+                    int proxyPort;
+                    try {
+                        proxyPort = Integer.parseInt(proxyAddressParts[1]);
+                        
+                    } catch (Exception exc) {
+                        throw new IllegalArgumentException("Proxy port specified was not a number.");
+                    }
+                    
+                    HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+                    
+                    // set proxy credentials if provided
+                    if (proxyUsername != null) {
+                        if (proxyPassword == null)
+                            throw new IllegalArgumentException("Proxy user");
+                        
+                        _credentialsProvider.setCredentials(new AuthScope(proxy),
+                                new UsernamePasswordCredentials(proxyUsername, proxyPassword));
+                    }
+                    
+                    httpClientBuilder.setRoutePlanner(new DefaultProxyRoutePlanner(proxy));
+                }
+                
+                _httpClient = httpClientBuilder.build();
+            }
+        }
     }
     
-    public class SpecialDefaultClient extends DefaultHttpClient {
-        
-        @Override
-        protected HttpParams createHttpParams() {
-            BasicHttpParams params = new BasicHttpParams();
-            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
-            
-            HttpProtocolParams.setUserAgent(params, Nodel.getAgent());
-
-            HttpConnectionParams.setConnectionTimeout(params, 15000);
-            HttpConnectionParams.setSoTimeout(params, 15000);
-            
-            // this suppresses some excessive logging related to cookies on some sites
-            params.setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
-            
-            return params;
-        }        
-        
-        @Override
-        protected ClientConnectionManager createClientConnectionManager() {
-            SchemeRegistry registry = new SchemeRegistry();
-            
-            registry.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
-            registry.register(new Scheme("https", 443, SSLSocketFactory.getSocketFactory()));
-            ThreadSafeClientConnManager connManager = new ThreadSafeClientConnManager(registry);
-            
-            return connManager;
-        }
-        
-        @Override
-        protected AuthSchemeRegistry createAuthSchemeRegistry() {
-            AuthSchemeRegistry registry = super.createAuthSchemeRegistry();
-            
-            // add support for NTLM
-            registry.register("ntlm", NTLMSchemeFactory.instance());
-            
-            return registry;
-        }
-        
-        
-    }
-
     @Override
     public String makeRequest(String urlStr, Map<String, String> query, 
                          String username, String password, 
@@ -107,23 +119,39 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
                          String post, 
                          Integer connectTimeout, Integer readTimeout,
                          String proxyAddress, String proxyUsername, String proxyPassword) {
+        
+        // sets up proxy *only* if it's the first the request
+        tryInitHttpClient(proxyAddress, proxyUsername, proxyPassword);
+        
         // record rate of new connections
         s_attemptRate.incrementAndGet();
-
+        
         // construct the full URL (includes query string)
         String fullURL = buildQueryString(urlStr, query);
 
         // (out of scope for clean up purposes)
         InputStream inputStream = null;
-
+        
+        URI uri;
+        try {
+            uri = new URI(fullURL);
+            
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        
         try {
             s_activeConnections.incrementAndGet();
             
+            // if username is supplied, apply security
+            if (!Strings.isBlank(username))
+                applySecurity(uri, username, !Strings.isEmpty(password) ? password : "");
+            
             // 'get' or 'post'?
-            HttpRequestBase request = (post == null ? new HttpGet(fullURL) : new HttpPost(fullURL));
+            HttpRequestBase request = (post == null ? new HttpGet(uri) : new HttpPost(uri));
             
             // set 'Content-Type' header
-            if (!Strings.isNullOrEmpty(contentType))
+            if (!Strings.isBlank(contentType))
                 request.setHeader("Content-Type", contentType);
 
             // add (or override) any request headers
@@ -132,54 +160,20 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
                     request.setHeader(entry.getKey(), entry.getValue());
             }
             
-            if (!Strings.isNullOrEmpty(post)) {
+            if (!Strings.isEmpty(post)) {
                 HttpPost httpPost = (HttpPost) request;
                 httpPost.setEntity(new StringEntity(post));
             }
-            
-            // if username is supplied, apply security
-            if (!Strings.isNullOrEmpty(username))
-                applySecurity(_httpClient, request, username, !Strings.isNullOrEmpty(password) ? password : "");
             
             // set any timeouts that apply
             if (connectTimeout != null || readTimeout != null) {
                 int actualConnTimeout = connectTimeout != null ? connectTimeout : DEFAULT_CONNECTTIMEOUT;
                 int actualReadTimeout = readTimeout != null ? readTimeout : DEFAULT_READTIMEOUT;
 
-                // there are a few places where timeouts can be set within this framework
-                // (this may be more than needed)
-                HttpConnectionParams.setConnectionTimeout(request.getParams(), actualConnTimeout);
-                HttpConnectionParams.setSoTimeout(request.getParams(), actualReadTimeout);
-            }
-            
-            // using a proxy?
-            if (proxyAddress != null) {
-                String[] proxyAddressParts = proxyAddress.split(":");
-                if (proxyAddressParts.length != 2)
-                    throw new IllegalArgumentException("Proxy address is not in form host:port");
-
-                String proxyHost = proxyAddressParts[0];
-                int proxyPort;
-                try {
-                    proxyPort = Integer.parseInt(proxyAddressParts[1]);
-                    
-                } catch (Exception exc) {
-                    throw new IllegalArgumentException("Port specified was not a number.");
-                }
-
-                // set proxy credentials if provided
-                if (proxyUsername != null) {
-                    if (proxyPassword == null)
-                        throw new IllegalArgumentException("Proxy user");
-                        
-                    _httpClient.getCredentialsProvider().setCredentials(new AuthScope(proxyHost, proxyPort),
-                            new UsernamePasswordCredentials(proxyUsername, proxyPassword));
-                }
-
-                // force the proxy
-                // NOTE: 'Request params' are not used, instead the proxy applies to the HTTP client params
-                HttpHost proxy = new HttpHost(proxyHost, proxyPort);
-                _httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+                request.setConfig(RequestConfig.copy(_requestConfig)
+                        .setConnectTimeout(actualConnTimeout)
+                        .setSocketTimeout(actualReadTimeout)
+                        .build());
             }
             
             // perform the request
@@ -187,7 +181,7 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
             httpResponse = _httpClient.execute(request);
             
             // count the post now
-            if (!Strings.isNullOrEmpty(post))
+            if (!Strings.isEmpty(post))
                 s_sendRate.addAndGet(post.length());
             
             // safely get the response (regardless of response code for now)
@@ -205,7 +199,7 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
             InputStreamReader isr = null;
             
             // try using the given encoding
-            if (!Strings.isNullOrEmpty(contentEncoding)) {
+            if (!Strings.isBlank(contentEncoding)) {
                 // any unknown content encodings will cause an exception to propagate
                 isr = new InputStreamReader(inputStream,  contentEncoding);
             } else {
@@ -224,7 +218,7 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
             } else {
                 // raise an exception including the content
                 throw new IOException(String.format("Server returned '%s' with content %s", 
-                        statusLine, Strings.isNullOrEmpty(content) ? "<empty>" : JSONObject.quote(content)));
+                        statusLine, Strings.isEmpty(content) ? "<empty>" : JSONObject.quote(content)));
             }
         } catch (IOException exc) {
             throw new UnexpectedIOException(exc);
@@ -286,7 +280,7 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
     /**
      * Applies security for a given HTTP request.
      */
-    private static void applySecurity(DefaultHttpClient client, HttpRequestBase httpRequest, String username, String password) {
+    private void applySecurity(URI location, String username, String password) {
         // in case of NTLM, check for "\" in username
         String userPart = username;
         String domainPart = null;
@@ -300,19 +294,20 @@ public class ApacheNodelHttpClient extends NodelHTTPClient {
             UsernamePasswordCredentials creds = new UsernamePasswordCredentials(username, password);
 
             // basic or digest or whatever
-            client.getCredentialsProvider().setCredentials(new AuthScope(httpRequest.getURI().getHost(), httpRequest.getURI().getPort(), AuthScope.ANY_SCHEME), creds);
+            _credentialsProvider.setCredentials(new AuthScope(location.getHost(), location.getPort(), AuthScope.ANY_SCHEME), creds);
 
-            httpRequest.addHeader(BasicScheme.authenticate(creds, "US-ASCII", false));
+            // TODO: REMOVE IF UNNCESSARY
+            // httpRequest.addHeader(BasicScheme.authenticate(creds, "US-ASCII", false));
         } else {
             // normally used with NTLM
-            client.getCredentialsProvider().setCredentials(new AuthScope(httpRequest.getURI().getHost(), httpRequest.getURI().getPort(), AuthScope.ANY_SCHEME), 
+            _credentialsProvider.setCredentials(new AuthScope(location.getHost(), location.getPort(), AuthScope.ANY_SCHEME), 
                     new NTCredentials(userPart, password, "WORKSTATION", domainPart));
         }
     }    
 
     @Override
     public void close() throws IOException {
-        _httpClient.getConnectionManager().shutdown();
+        _httpClient.close();
     }        
     
 }
